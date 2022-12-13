abf53b9fa628fa93c9c6da01f4b9b98698768838
akshay <00akshay2403@gmail.com>
2022-12-13 13:13:11 +0530

    extends main class commit

--- src/main/java/com/actuator/demo/BasicController.java
+++ src/main/java/com/actuator/demo/BasicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicController {
 
-	@GetMapping("/hello")
+	@GetMapping("/get")
 	public String getData() {
 		return "WelcomeToLoveToLearn";
 	}
--- src/main/java/com/actuator/demo/JenkinsSpringBootApplication.java
+++ src/main/java/com/actuator/demo/JenkinsSpringBootApplication.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsSpringBootApplication {
+public class JenkinsSpringBootApplication extends SpringBootServletInitializer{
 	
 	public static Logger logger = LoggerFactory.getLogger(JenkinsSpringBootApplication.class);
 	
@@ -22,5 +24,10 @@ public class JenkinsSpringBootApplication {
 		logger.info("Application executed ........");
 		SpringApplication.run(JenkinsSpringBootApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(JenkinsSpringBootApplication.class);
+	  }
 
 }
